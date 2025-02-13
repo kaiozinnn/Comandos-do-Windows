8fe101090a7bbe1529d91086edc7b7dba1eaa633
Kaio Vinícius <96919635+kaiozinnn@users.noreply.github.com>
2025-02-13 14:24:33 -0300

    Executar.bat

--- Executar.bat
+++ Executar.bat
@@ -1,6 +1,20 @@
-mkdir filmes
-rmdir filmes
-echo. > links.txt
-del links.txt
-cd ..
-echo Pasta filmes excluido!
+mkdir Kaio
+pause
+rmdir Kaio
+mkdir Vinicius
+pause
+cd Vitor
+echo. >Breno.txt
+copy Breno.txt Vitor
+pause 
+cd .. 
+mkdir Gabriel 
+move Vitor\Breno.txt Gabriel\Bryan.txt
+pause 
+cd Gabriel
+rename Bryan.txt Gustavo.txt
+pause
+del Gustavo.txt
+echo ola Gustavo, como vc esta. >Gustavo.txt 
+type Gustavo.txt
+pause
